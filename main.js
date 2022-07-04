7aed510e12ce9e99ec2fddfc2ca2fdb45581cbaf
miss-gif <ddc.akari@gmail.com>
2022-07-04 18:54:06 +0900

    정규표현식 1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("확인");
+const str = `
+010-1234-1234
+abc@naver.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+aaabbbcccdddd est EST
+`;
+
+// const regexp = new RegExp("est", "gi"); // g 모두 찾기 i 대소문자 구분 하지 않음
+const regexp = /est/gi;
+console.log(str.match(regexp));
