de8b1095666a17d46659da20255aeab71a491b71
miss-gif <ddc.akari@gmail.com>
2022-07-05 22:49:01 +0900

    정규표현식 3

--- README.md
+++ README.md
@@ -43,3 +43,11 @@ aaabbbcccdddd est EST
 | test    | 정규식.test(문자열)             | 일치 여부(Boolean) 반환                               |
 | match   | 문자열.match(정규식)            | 일치하는 문자열의 배열(Array) 반환                    |
 | replace | 문자열.replace(정규식,대체문자) | 일치하는 문자열을 대체하고 대체된 문자열(String) 반환 |
+
+## 플래그(옵션)
+
+| 플래그 | 설명                                                     |
+| ------ | -------------------------------------------------------- |
+| g      | 모든 문자와 여러 줄 일치(global)                         |
+| i      | 영어 대소문자를 구분 않고 일치(insensitive, ignore case) |
+| m      | 여러 줄 일치(multi line)                                 |
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let str = `
-010-1234-1234
+010-1234-1234.
 abc@naver.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
-Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
+"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
 aaabbbcccdddd est EST
 `;
 
 // const regexp = new RegExp("est", "gi"); // g 모두 찾기 i 대소문자 구분 하지 않음
-const regexp = /aaa/gi;
-// console.log(str.match(regexp));
-// console.log(regexp.test(str));
-str = str.replace(regexp, "ABC");
-console.log(str);
+// const regexp = /porro/gi;
+console.log(str.match(/\.$/gim));
+
+// 정규 표현식에서 .(마침표)는 특정한 문자를 검색하는 패턴이다.
+// 여기서 사용하고자 하는 것은 문자로서의 .(마침표)임으로 이스케이프 문자화한다.
